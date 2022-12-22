4c276479093bdf2e9b8337bfbd42467a06bdd898
GicomoSeveri <giacomo.severi.sg@gmail.com>
2022-12-22 16:11:30 +0100

    besic slider finish

--- js/script.js
+++ js/script.js
@@ -9,3 +9,37 @@ const src = document.querySelectorAll('.gallery img');
 let currentIndex = 0;
 src[currentIndex].classList.add('active');
 
+// ! funzionamento scorri foto
+// aggiungo l'evento al bottone next
+next.addEventListener('click', function(){
+   // rimuovo la classe active
+   src[currentIndex].classList.remove('active');
+   
+   // incremento l'indice
+    currentIndex++;
+
+    // valido la fine delle foto
+    if(currentIndex === src.length){
+        currentIndex = 0;
+    }
+
+    // aggiungo una nuova classe active
+    src[currentIndex].classList.add('active');
+})
+
+// aggiungo l'evento al bottone prev
+prev.addEventListener('click', function(){
+    // rimuovo la classe active
+    src[currentIndex].classList.remove('active');
+    
+    // decremento l'indice
+     currentIndex--;
+ 
+     // valido l'inizio delle foto
+      if(currentIndex < 0){
+          currentIndex = src.length - 1;
+      }
+ 
+     // aggiungo una nuova classe active
+     src[currentIndex].classList.add('active');
+ })
