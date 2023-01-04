16f8f6b4a86ff2fa67eb1f18dca44c636cf470dd
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-01-04 18:28:24 +0100

    function added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,54 @@
-const data = ['img/01.webp','img/02.webp','img/03.webp','img/04.webp','img/05.webp',]
+function changePic(target){
+   // rimuovo la classe active
+   src[currentIndex].classList.remove('active');
+   thumbSrc[currentIndex].classList.remove('active');
+   
+   if(target === 'next'){
+   // incremento l'indice
+    currentIndex++;
+
+    // valido la fine delle foto
+    if(currentIndex === src.length){
+        currentIndex = 0;
+    }
+   } else if(target === 'prev'){
+    // decremento l'indice
+    currentIndex--;
+ 
+    // valido l'inizio delle foto
+     if(currentIndex < 0){
+         currentIndex = src.length - 1;
+     }
+   }
+
+    // aggiungo una nuova classe active
+    src[currentIndex].classList.add('active');
+    thumbSrc[currentIndex].classList.add('active');
+}
+
+const data = [
+    {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+  ];
 
 // ! recupero gli elemnti 
 // bottoni
@@ -10,12 +60,18 @@ const tumbGallery = document.getElementById('thumb')
 
 // ! creo un ciclo per stampare le foto
 let banner = '';
+let thumbElement = '';
 for(let i = 0; i < data.length; i++){
-    banner += `<img src="${data[i]}" alt="${data[i]}">`;
+    banner += `<img src="${data[i].image}" alt="${data[i].title}">
+               <h2>${data[i].title}</h2>`;
+}
+
+for(let i = 0; i < data.length; i++) {
+  thumbElement += `<img src="${data[i].image}" alt="${data[i].title}">`;
 }
 
 gallery.innerHTML = banner;
-tumbGallery.innerHTML = banner;
+tumbGallery.innerHTML = thumbElement;
 
 const src = document.querySelectorAll('.gallery img');
 const thumbSrc = document.querySelectorAll('#thumb img');
@@ -29,38 +85,12 @@ thumbSrc[currentIndex].classList.add('active');
 // ! funzionamento scorri foto
 // aggiungo l'evento al bottone next
 next.addEventListener('click', function(){
-   // rimuovo la classe active
-   src[currentIndex].classList.remove('active');
-   thumbSrc[currentIndex].classList.remove('active');
-   
-   // incremento l'indice
-    currentIndex++;
-
-    // valido la fine delle foto
-    if(currentIndex === src.length){
-        currentIndex = 0;
-    }
-
-    // aggiungo una nuova classe active
-    src[currentIndex].classList.add('active');
-    thumbSrc[currentIndex].classList.add('active');
-})
+   // richiamo la funzione 
+   changePic('next');
+});
 
 // aggiungo l'evento al bottone prev
 prev.addEventListener('click', function(){
-    // rimuovo la classe active
-    src[currentIndex].classList.remove('active');
-    thumbSrc[currentIndex].classList.remove('active');
-    
-    // decremento l'indice
-     currentIndex--;
- 
-     // valido l'inizio delle foto
-      if(currentIndex < 0){
-          currentIndex = src.length - 1;
-      }
- 
-     // aggiungo una nuova classe active
-     src[currentIndex].classList.add('active');
-     thumbSrc[currentIndex].classList.add('active');
-})
+     // richiamo la funzione 
+   changePic('prev');
+});
